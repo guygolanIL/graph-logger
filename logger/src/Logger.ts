b53d0d2fea662293574d893fe69e28669ca32fc9
guy.golan <guygolan91@gmail.com>
2019-12-28 14:10:36 +0200

    added log dumping

--- logger/src/Logger.ts
+++ logger/src/Logger.ts
@@ -1,13 +1,21 @@
 import { GraphLog } from "./entities/GraphLog";
 import { LoggerConfiguration } from "./config/LoggerConfiguration";
+import { existsSync, exists, readFile, writeFile } from "fs";
+import { execFileSync } from "child_process";
 
 export class Logger {
     private static INSTANCE: Logger = new Logger();
 
+    private static LOG_FILE_NAME: string = "data.json";
+
     private config: LoggerConfiguration = LoggerConfiguration.getInstance();
     private graphLogs: Array<GraphLog> = [];
+    private loggingInterval: NodeJS.Timeout;
 
-    private constructor(){}
+    private constructor() {
+        console.log("setting logging interval");
+        this.loggingInterval = this.setLoggingInterval();
+    }
 
     public static getInstance(): Logger {
         return this.INSTANCE;
@@ -19,5 +27,48 @@ export class Logger {
 
     public getLogs(): Array<GraphLog> {
         return this.graphLogs;
+    }
+
+    private setLoggingInterval() {
+        return setInterval(() => {
+            if (this.graphLogs.length > this.config.getWritingThreshold()) {
+                const saveLocation: string = this.config.getWritingLocation();
+                exists(saveLocation, (exists: boolean) => {
+                    if (exists) {
+                        readFile(saveLocation, (err, data: any) => {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                const jsonData = JSON.parse(data);
+                                jsonData.data = [
+                                    ...jsonData.data,
+                                    ...this.graphLogs
+                                ];
+                                this.graphLogs = [];
+                                writeFile(
+                                    saveLocation,
+                                    JSON.stringify(jsonData),
+                                    () => {
+                                        console.log("Writing completed");
+                                    }
+                                );
+                            }
+                        });
+                    } else {
+                        const jsonToSave = {
+                            data: [...this.graphLogs]
+                        };
+                        this.graphLogs = [];
+                        writeFile(
+                            saveLocation,
+                            JSON.stringify(jsonToSave),
+                            () => {
+                                console.log("Initial writing completed");
+                            }
+                        );
+                    }
+                });
+            }
+        }, this.config.getWritingTimeout());
     }
 }
--- logger/src/config/LoggerConfiguration.ts
+++ logger/src/config/LoggerConfiguration.ts
@@ -4,6 +4,7 @@ import * as path from "path";
 export class LoggerConfiguration {
     private static CONFIGURATION_FILE_NAME: string = "graph-logger.json";
     private static INSTANCE: LoggerConfiguration = new LoggerConfiguration();
+
     private configJsonFromFile: any = null;
 
     private constructor() {
@@ -23,6 +24,37 @@ export class LoggerConfiguration {
         } catch (error) {
             console.log(error);
         }
+    }
+
+    private getProperty(prop: string): string | undefined{
+        return this.configJsonFromFile[prop];
+    }
+
+    private getPropIfExists(propKey: string, ifExistsCb: (propValue: string) => any, ifNotExistsCb: Function){
+        const propValue = this.getProperty(propKey);
+        if(propValue){
+            return ifExistsCb(propValue);
+        } else {
+            return ifNotExistsCb();
+        }
+    }
+
+    public getWritingLocation(): string {
+        const DEFAULT_LOCATION: string = "data.json";
+        const writingLocationKey: string = "writingLocation";
+        return this.getPropIfExists(writingLocationKey, (propValue: string) => propValue, () => DEFAULT_LOCATION);
+    }
+
+    public getWritingTimeout(): number {
+        const DEFAULT_TIMEOUT: number = 100
+        const writingTimeoutKey = "writingTimeout";
+        return this.getPropIfExists(writingTimeoutKey, (propValue: string) => parseInt(propValue), () => DEFAULT_TIMEOUT);
+    }
+
+    public getWritingThreshold(): number {
+        const DEFAULT_THRESHOLD: number = 100;
+        const writingThresholdKey = "writingThreshold";
+        return this.getPropIfExists(writingThresholdKey, (propValue: string) => parseInt(propValue), () => DEFAULT_THRESHOLD);
     }
 
     public static getInstance(): LoggerConfiguration {
